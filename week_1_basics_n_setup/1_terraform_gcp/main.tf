472a6c1f99ecb8f7742d1cb1b5d3f9b88a43c959
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2023-09-13 12:00:19 -0400

    Minimal terraform IaC definition.

--- week_1_basics_n_setup/1_terraform_gcp/main.tf
+++ week_1_basics_n_setup/1_terraform_gcp/main.tf
@@ -8,16 +8,22 @@ terraform {
   }
 }
 
+variable "project" {
+  type        = string
+  default     = "evocative-tide-398716"
+  description = "Globally unique project id"
+}
+
 provider "google" {
-    project = "evocative-tide-398716"
+    project = var.project
     region  = "us-east4" # N. Virginia
     zone    = "us-east4-a" # See regions and zones at https://cloud.google.com/compute/docs/regions-zones
 }
 
 # Data Lake Bucket
 # Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
-resource "google_storage_bucket" "data_lake_bucket" {
-  name          = "data-lake-bucket"
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = "dtc-data-lake-bucket_${var.project}"
   location      = "us-east4"
   storage_class = "STANDARD"
   force_destroy = true
@@ -29,7 +35,14 @@ resource "google_storage_bucket" "data_lake_bucket" {
       type = "Delete"
     }
     condition {
-      age = 30
+      age = 30 # days
     }
   }
+}
+
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "trips_data_all"
+  location   = "us-east4"
+  default_table_expiration_ms = 3600000 # auto delete table after 1 hour
 }
