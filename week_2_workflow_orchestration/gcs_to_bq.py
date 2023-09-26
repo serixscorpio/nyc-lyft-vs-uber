4d722d6546342ae606dbe45f2d97d27ef84a18d1
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2023-09-26 14:58:52 -0400

    Avoid the need for a temporary file.

--- week_2_workflow_orchestration/gcs_to_bq.py
+++ week_2_workflow_orchestration/gcs_to_bq.py
@@ -1,3 +1,5 @@
+from io import BytesIO
+
 import pandas as pd
 from prefect import flow
 from prefect_gcp.cloud_storage import GcsBucket
@@ -9,11 +11,13 @@ def gcs_to_bq():
     orchestrate gcs_to_bq
     """
     # input bucket name + gcp credentials (implicit)
-    gcs_path = GcsBucket(
-        bucket="dtc-tfstate-bucket_evocative-tide-398716"
-    ).download_object_to_path("yellow_tripdata_2023-06.parquet")
+    gcs_bucket = GcsBucket(bucket="dtc-tfstate-bucket_evocative-tide-398716")
+    with BytesIO() as buf:
+        gcs_bucket.download_object_to_file_object(
+            "yellow_tripdata_2023-06.parquet", buf
+        )
+        df: pd.DataFrame = pd.read_parquet(buf)
     # parquet file to panda dataframe
-    df = pd.read_parquet(gcs_path)
     # fill in zero passengers
     print(f"pre: missing passenger count: {df.passenger_count.isna().sum()}")
     df.passenger_count.fillna(0, inplace=True)
