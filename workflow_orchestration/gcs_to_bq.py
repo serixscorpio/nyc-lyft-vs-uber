82425429522ffcc27767700d0d5efddb0f584ce0
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2023-10-23 11:44:47 -0400

    Retrieve high volume for hire service data.
    Target high volume for hire service (hvfhs) data instead of yellow taxi
    data for now.  Only retrieve relevant columns for analyses to reduce
    storage costs.

--- workflow_orchestration/gcs_to_bq.py
+++ workflow_orchestration/gcs_to_bq.py
@@ -13,36 +13,20 @@ def gcs_to_bq(file_name: str) -> None:
     """
     # input bucket name + gcp credentials (implicit)
     gcs_bucket = GcsBucket(bucket="dtc-tfstate-bucket_evocative-tide-398716")
+    # parquet file to panda dataframe
     with BytesIO() as buf:
         gcs_bucket.download_object_to_file_object(file_name, buf)
-        df: pd.DataFrame = pd.read_parquet(buf)
-    # parquet file to panda dataframe
-    # fill in zero passengers
-    print(f"pre: missing passenger count: {df.passenger_count.isna().sum()}")
-    df.passenger_count.fillna(0, inplace=True)
-    print(f"post: missing passenger count: {df.passenger_count.isna().sum()}")
-    # 3. panda dataframe to bigquery
-    # approach 2: use bigquery client
+        df: pd.DataFrame = pd.read_parquet(buf, columns=["hvfhs_license_num", "driver_pay", "pickup_datetime"])
+    # load dataframe into bigquery
     bigquery.Client().load_table_from_dataframe(
         df,
         "evocative-tide-398716.dtc_nyc_trip_data.rides",
         job_config=bigquery.LoadJobConfig(
-            write_disposition="WRITE_TRUNCATE",
+            write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
             time_partitioning=bigquery.TimePartitioning(
                 type_=bigquery.TimePartitioningType.DAY,
-                field="tpep_pickup_datetime",
+                field="pickup_datetime",
             ),
-            # range_partitioning=bigquery.table.RangePartitioning(
-            #     field="payment_type",
-            #     range_=bigquery.table.PartitionRange(start=1, end=7, interval=1),
-            # ),
-            # clustering_fields=["tpep_pickup_datetime"],
-            clustering_fields=["payment_type"],
+            clustering_fields=["hvfhs_license_num"],
         ),
     )
-    # approach 1: use pandas-gbq
-    # df.to_gbq(
-    #     destination_table="dtc_nyc_trip_data.rides",
-    #     project_id="evocative-tide-398716",
-    #     if_exists="append",
-    # )
--- workflow_orchestration/local_deploy.py
+++ workflow_orchestration/local_deploy.py
@@ -3,7 +3,7 @@ from prefect.deployments import Deployment, run_deployment
 from web_to_bq import web_to_bq
 
 if __name__ == "__main__":
-    file_name = "yellow_tripdata_2023-02.parquet"
+    file_name = "fhvhv_tripdata_2023-07.parquet"
     Deployment.build_from_flow(web_to_bq, name="web_to_bq", apply=True, work_pool_name="local-process")
     run_deployment(
         name="web-to-bq/web_to_bq",
