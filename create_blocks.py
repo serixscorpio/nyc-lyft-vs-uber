1b8a1df7019a75e2c31edc7e807161cd3c6cd5be
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2023-11-01 14:50:27 -0400

    Create dbt cli profile block.

--- create_blocks.py
+++ create_blocks.py
@@ -1,6 +1,9 @@
+"""Create Prefect Blocks"""
 import os
 
 from dotenv import load_dotenv
+from prefect_dbt.cli import DbtCliProfile
+from prefect_dbt.cli.configs import BigQueryTargetConfigs
 from prefect_gcp.credentials import GcpCredentials
 
 load_dotenv()
@@ -12,3 +15,11 @@ def save_block(block, name: str = "default") -> None:
 
 gcp_credentials = GcpCredentials(service_account_file=os.getenv("GCP_SERVICE_ACCOUNT_FILE"))
 save_block(gcp_credentials)
+
+
+dbt_cli_profile = DbtCliProfile(
+    name="dbt_dwh_models",
+    target="default",
+    target_configs=BigQueryTargetConfigs(schema=os.getenv("GCP_BIGQUERY_SCHEMA"), credentials=gcp_credentials),
+)
+save_block(dbt_cli_profile)
